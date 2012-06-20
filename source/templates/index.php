a3578584041dcb8c2a5ad87cf6782b40fe5b448c
Joshua Abbott <joshnabbott@gmail.com>
2012-06-19 21:28:15 -0700

    Roughly started blog post index (index.php)

--- source/templates/index.php
+++ source/templates/index.php
@@ -1,4 +1,137 @@
 <?php get_header(); ?>
-<?php get_template_part( 'loop' ); ?>
-<?php get_sidebar(); ?>
+
+<div id="intro">
+  <div class="wrap">
+    <div class="c-8">
+      <h1>Blog Posts</h1>
+      <p class="breadcrumbs">You are here: <a href="home.html">Home</a> &raquo; <strong>Blog Posts</strong> </p>
+    </div>
+
+    <div class="c-4">
+      <ul class="social-bookmarking">
+        <li><a class="tw" href=""></a></li>
+          <li><a class="fb" href=""></a></li>
+          <li><a class="in" href=""></a></li>
+          <li><a class="rss" href=""></a></li>
+      </ul>
+    </div>
+  </div><!-- end wrap -->
+</div><!-- end intro -->
+
+<div id="content">
+  <div class="wrap">
+    <div class="c-8 divider">
+      <div class="post-list">
+        <div class="post">
+          <h2 class="title"><a href="">H2 Lorem ipsum dolor sit amet consect.</a></h2>
+          <p class="meta">
+            <span>Date: <a class="date" title="" href="#">15 Feb 2010</a></span>
+            <span>Author: <a class="author" title="" href="#">admin</a></span>
+            <span class="categories">Categories: <a class="category" title="" href="#">Category1</a>, <a class="category" href="#">Category2</a></span>
+            <span class="comments"><a title="" href="#">13 </a></span>
+          </p>
+          <p class="image"><a href=""><img src="assets/post-thumb-606x262.jpg" width="582" height="262" alt="" title="" /></a></p>
+          <div class="excerpt">
+            <p><strong>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem vehicula vitae. facilisis vel, accumsan a, sapien. Vivamus convallis rutrum lorem.
+Praesent massa lectus, vulputate sed, vestibulum eu, vestibulum eu, dolor. Integer ante. Proin ac odio in urna cursus ultricies. Quisque tincidunt lacus in ante. Donec adipiscing. </strong></p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+          <p class="meta dashed">
+            <span class="tags">Tags: <a class="tag" title="" href="#">Tag 1</a>, <a class="category" href="#">Tag 2</a></span>
+          </p>
+        </div><!-- end post -->
+
+        <div class="post">
+          <h2 class="title"><a href="">H2 Lorem ipsum dolor sit amet consect.</a></h2>
+          <p class="meta">
+            <span>Date: <a class="date" title="" href="#">15 Feb 2010</a></span>
+            <span>Author: <a class="author" title="" href="#">admin</a></span>
+            <span class="categories">Categories: <a class="category" title="" href="#">Category1</a>, <a class="category" href="#">Category2</a></span>
+            <span class="comments"><a title="" href="#">13 </a></span>
+          </p>
+          <p class="image"><a href=""><img src="assets/post-thumb-606x262.jpg" width="582" height="262" alt="" title="" /></a></p>
+          <div class="excerpt">
+            <p><strong>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem vehicula vitae. facilisis vel, accumsan a, sapien. Vivamus convallis rutrum lorem.
+Praesent massa lectus, vulputate sed, vestibulum eu, vestibulum eu, dolor. Integer ante. Proin ac odio in urna cursus ultricies. Quisque tincidunt lacus in ante. Donec adipiscing. </strong></p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+          <p class="meta dashed">
+            <span class="tags">Tags: <a class="tag" title="" href="#">Tag 1</a>, <a class="category" href="#">Tag 2</a></span>
+          </p>
+        </div><!-- end post -->
+        <div class="pagination">
+          <ul>
+            <li><a href="#" class="previous">Previous</a></li>
+            <li><a href="#" class="current">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#" class="next">Next</a></li>
+          </ul>
+        </div><!-- end pagination -->
+      </div><!-- end post-list -->
+    </div>
+
+    <div class="c-4 sidebar">
+      <div class="widget widget-today-special">
+        <h3 class="widget-title">This Week Special</h3>
+        <p class="image"><img width="142" height="142" alt="" src="assets/thumb-specials-142x142.jpg" /></p>
+        <h2 class="title"><a title="" href="">Easy Cheesy Touchdown Bean</a></h2>
+        <div class="excerpt">
+          <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. </p>
+        </div>
+        <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+      </div><!-- end widget-today-special -->
+
+      <div class="widget widget-news-events">
+        <h3 class="widget-title">News and Events</h3>
+        <ul>
+          <li>
+            <p class="meta">10 APRIL | 2011</p>
+            <h3 class="title"><a title="" href="">Suspendisse Dictum Suscipit Odio</a></h3>
+            <div class="excerpt">
+              <p>Suspendisse dictu m suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies. Nulla dui arcu. </p>
+            </div>
+          </li>
+          <li>
+            <p class="meta">25 MAY | 2011</p>
+            <h3 class="title"><a title="" href="">Suspendisse Dictum Suscipit Odio</a></h3>
+            <div class="excerpt">
+              <p>Suspendisse dictu m suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies. Nulla dui arcu. </p>
+            </div>
+          </li>
+        </ul>
+      </div><!-- end widget-news-events-->
+
+      <div class="widget widget-restaurant-hours">
+        <h3 class="widget-title">Restaurant Hours</h3>
+        <ul class="restaurant-hours">
+          <li><p class="meta">Breakfast</p></li>
+          <li>
+            <label>Monday - Friday</label>
+            <span>11:00 am - 03:00 pm</span>
+          </li>
+          <li>
+            <label>Monday - Friday</label>
+            <span>11:00 am - 03:00 pm</span>
+          </li>
+        </ul>
+        <ul class="restaurant-hours">
+          <li><p class="meta">Lunch</p></li>
+          <li>
+            <label>Monday - Friday</label>
+            <span>11:00 am - 03:00 pm</span>
+          </li>
+          <li>
+            <label>Monday - Friday</label>
+            <span>11:00 am - 03:00 pm</span>
+          </li>
+        </ul>
+      </div><!-- end widget-restaurant-hours -->
+    </div><!-- end sidebar -->
+  </div><!-- end wrap -->
+</div><!-- end content -->
+
 <?php get_footer(); ?>
+
