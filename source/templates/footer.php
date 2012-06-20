f49928d4338771f8a4a714ea2159631e261ac046
Joshua Abbott <joshnabbott@gmail.com>
2012-06-19 19:46:28 -0700

    Moved footer over.

--- source/templates/footer.php
+++ source/templates/footer.php
@@ -1,7 +1,78 @@
-    <footer>
-      <p><a href="http://wordpress.org/" rel="generator">Proudly powered by WordPress</a></p>
-    </footer>
-    <?php wp_footer(); ?>
+    <div id="footer">
+      <div class="wrap">
+        <div class="c-4">
+          <div class="widget widget-about">
+            <h3 class="widget-title">Chef Mazzareli</h3>
+            <p class="image"><img src="<?php bloginfo('template_url'); ?>/images/about-ft-82x82.jpg" width="82" height="82" alt="" /></p>
+            <div class="excerpt">
+              <p>Suspendisse dictu m suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan a, sapien. Vivamus convallis rutrum. Suspendisse dictu m suscipit odio. Nulla dui arcu, fermentum quis, mattis quis. ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan a, sapien. </p>
+            </div>
+            <p class="actions"><a class="read-more-white" href="" title="">Read more</a></p>
+          </div><!-- end widget-about --> 
+        </div>
+        <div class="c-4">
+          <div class="widget  widget-menu-highlights">
+            <h3 class="widget-title">Menu Highlights</h3>
+            <ul>
+              <li>
+                <p class="image"><img src="<?php bloginfo('template_url'); ?>/images/menu-ft-56x56.jpg" width="56" height="56" alt="" /></p>
+                <h2 class="widget-title"><a href="" title="">French Wedge</a></h2>
+                <div class="excerpt">
+                  <p>Suspendisse dictu m suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies. </p>
+                </div>
+              </li>
+              <li>
+                <p class="image"><img src="<?php bloginfo('template_url'); ?>/images/menu-ft-56x56-1.jpg" width="56" height="56" alt="" /></p>
+                <h2 class="widget-title"><a href="" title="">Tarte aux pommes</a></h2>
+                <div class="excerpt">
+                  <p>Suspendisse dictu m suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies. </p>
+                </div>
+              </li>
+              <li>
+                <p class="actions"><a class="read-more-white" href="" title="">Look for more</a></p>
+              </li>
+            </ul>
+          </div><!-- end widget-menu-highlights --> 
+        </div>
+
+        <div class="c-4">
+          <div class="widget widget-contact-us">
+            <h3 class="widget-title">Contact Us</h3>
+            <form id="contactform" action="php/mail.php" method="post" enctype="multipart/form-data"> 
+              <p>
+                <label for="name">Your name:</label>
+                <input class="u-3" name="name" id="name" />
+              </p>
+              <p>
+                <label for="email">Your email:</label>
+                <input class="u-3" name="email" id="email" />
+              </p>
+              <p>
+                <label for="message">Message:</label>
+                <textarea class="u-3" name="message" id="message" rows="2" cols="1"></textarea>
+              </p>
+              <p>
+                <input type="submit" name="" class="submit" value="Contact Us" />
+              </p>
+            </form>
+          </div><!-- end widget-contact-us --> 
+        </div>
+      </div><!-- end wrap -->
+    </div><!-- end footer -->
+    <div id="subfooter">
+      <div class="wrap">
+        <div class="c-6">
+          <p id="copyright">&copy; 2011 onlinesolutions</p>
+        </div>
+        <div class="c-6">
+          <ul class="subfooter-menu">
+            <li><a href="" title="">Terms &amp; Conditions</a></li>
+            <li><a href="" title="">Copyrights</a></li>
+            <li><a href="" title="">Site Map</a></li>
+          </ul>
+        </div>
+      </div><!-- end wrap -->
+    </div><!-- end subfooter --> 
   </body>
 </html>
 
