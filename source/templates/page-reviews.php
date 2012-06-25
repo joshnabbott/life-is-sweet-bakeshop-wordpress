1dadac2eac09d0041bc6ea56d33a192b3dc9da97
Joshua Abbott <joshnabbott@gmail.com>
2012-06-24 20:37:15 -0700

    Added reviews page.

---
+++ source/templates/page-reviews.php
@@ -0,0 +1,62 @@
+<?php get_header(); ?>
+
+<div id="intro">
+  <div class="wrap">
+    <div class="c-8">
+      <h1>Reviews</h1>
+      <p class="breadcrumbs">You are here: <a href="home.html">Home</a> &raquo; <strong>Reviews</strong> </p>
+    </div>
+      
+    <div class="c-4">
+      <ul class="social-bookmarking">
+        <li><a class="tw" href=""></a></li>
+        <li><a class="fb" href=""></a></li>
+        <li><a class="in" href=""></a></li>
+        <li><a class="rss" href=""></a></li>
+      </ul>
+    </div>
+  </div><!-- end wrap -->
+</div><!-- end intro -->
+  
+<div id="content">
+  <div class="wrap">
+    <div class="c-8 divider">
+      <div class="review-list">
+          <div class=" review dashed">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan sapien. Vivamus convallis rutrumorem. Praesent massa lectus, vulputate sed, vestibulum eu, vestibulumeu. Integer ante. Donec imperdiet. Proin ac odio in urna cursus ultricies. Quisque tincidunt lacus in ante.  </p>
+            <p class="author"><span>Jonathan Doe</span> <br /> CFO, Company Name inc.</p>
+          </div><!-- end post -->
+                  
+          <div class=" review dashed">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan sapien. Vivamus convallis rutrumorem. Praesent massa lectus, vulputate sed, vestibulum eu, vestibulumeu. Integer ante. Donec imperdiet. Proin ac odio in urna cursus ultricies. Quisque tincidunt lacus in ante.  </p>
+            <p class="author"><span>Jonathan Doe</span> <br /> CFO, Company Name inc.</p>
+          </div><!-- end post -->
+                  
+          <div class=" review dashed">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan sapien. Vivamus convallis rutrumorem. Praesent massa lectus, vulputate sed, vestibulum eu, vestibulumeu. Integer ante. Donec imperdiet. Proin ac odio in urna cursus ultricies. Quisque tincidunt lacus in ante.  </p>
+            <p class="author"><span>Jonathan Doe</span> <br /> CFO, Company Name inc.</p>
+          </div><!-- end post -->
+                  
+          <div class=" review dashed">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan sapien. Vivamus convallis rutrumorem. Praesent massa lectus, vulputate sed, vestibulum eu, vestibulumeu. Integer ante. Donec imperdiet. Proin ac odio in urna cursus ultricies. Quisque tincidunt lacus in ante.  </p>
+            <p class="author"><span>Jonathan Doe</span> <br /> CFO, Company Name inc.</p>
+          </div><!-- end post -->
+                  
+          <div class=" review dashed">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan sapien. Vivamus convallis rutrumorem. Praesent massa lectus, vulputate sed, vestibulum eu, vestibulumeu. Integer ante. Donec imperdiet. Proin ac odio in urna cursus ultricies. Quisque tincidunt lacus in ante.  </p>
+            <p class="author"><span>Jonathan Doe</span> <br /> CFO, Company Name inc.</p>
+          </div><!-- end post -->
+                  
+          <div class=" review dashed">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan sapien. Vivamus convallis rutrumorem. Praesent massa lectus, vulputate sed, vestibulum eu, vestibulumeu. Integer ante. Donec imperdiet. Proin ac odio in urna cursus ultricies. Quisque tincidunt lacus in ante.  </p>
+            <p class="author"><span>Jonathan Doe</span> <br /> CFO, Company Name inc.</p>
+          </div><!-- end post -->
+        </div><!-- end post-list -->
+      </div>
+          
+      <?php get_sidebar(); ?>
+      </div><!-- end wrap -->
+  </div><!-- end content -->
+
+<?php get_footer(); ?>
+
