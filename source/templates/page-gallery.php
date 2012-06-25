6863e5158a789b4e37c4eb313c72886dec8bff64
Joshua Abbott <joshnabbott@gmail.com>
2012-06-24 20:49:59 -0700

    Added gallery page.

---
+++ source/templates/page-gallery.php
@@ -0,0 +1,201 @@
+<?php get_header(); ?>
+
+<div id="intro">
+  <div class="wrap">
+    <div class="c-8">
+      <h1>Gallery</h1>
+      <p class="breadcrumbs">You are here: <a href="home.html">Home</a> &raquo; <strong>Gallery</strong> </p>
+    </div>
+      
+    <div class="c-4">
+      <ul class="social-bookmarking">
+        <li><a class="tw" href=""></a></li>
+        <li><a class="fb" href=""></a></li>
+        <li><a class="in" href=""></a></li>
+        <li><a class="rss" href=""></a></li>
+      </ul>
+    </div>
+  </div><!-- end wrap -->
+</div><!-- end intro -->
+  
+<div id="content">
+  <div class="wrap">
+    <div class="c-12">
+      <ul class="portfolio-menu">
+        <li class="c-3 four-column">
+          <!-- <p class="meta"><a class="category" title="" href="#">Breakfast</a></p> -->
+          <!-- <h3 class="title"><a href="">Holiday Casseroles 1</a></h3> -->
+          <p class="image">                        	
+            <a class="lightbox" href="<?php bloginfo('template_url'); ?>/images/post-thumb-606x262-1.jpg">
+              <img height="114" width="195" title="" alt="" src="<?php bloginfo('template_url'); ?>/images/menu-4col-195x114.jpg" />
+            </a>
+          </p>
+          <div class="excerpt">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. </p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+        </li>
+        <li class="c-3 four-column">
+          <!-- <p class="meta"><a class="category" title="" href="#">Breakfast</a></p> -->
+          <!-- <h3 class="title"><a href="">Holiday Casseroles 1</a></h3> -->
+          <p class="image">                        	
+            <a class="lightbox" href="<?php bloginfo('template_url'); ?>/images/post-thumb-606x262-1.jpg">
+              <img height="114" width="195" title="" alt="" src="<?php bloginfo('template_url'); ?>/images/menu-4col-195x114.jpg" />
+            </a>
+          </p>
+          <div class="excerpt">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. </p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+        </li>
+        <li class="c-3 four-column">
+          <!-- <p class="meta"><a class="category" title="" href="#">Breakfast</a></p> -->
+          <!-- <h3 class="title"><a href="">Holiday Casseroles 1</a></h3> -->
+          <p class="image">                        	
+            <a class="lightbox" href="<?php bloginfo('template_url'); ?>/images/post-thumb-606x262-1.jpg">
+              <img height="114" width="195" title="" alt="" src="<?php bloginfo('template_url'); ?>/images/menu-4col-195x114.jpg" />
+            </a>
+          </p>
+          <div class="excerpt">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. </p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+        </li>
+        <li class="c-3 four-column">
+          <!-- <p class="meta"><a class="category" title="" href="#">Breakfast</a></p> -->
+          <!-- <h3 class="title"><a href="">Holiday Casseroles 1</a></h3> -->
+          <p class="image">                        	
+            <a class="lightbox" href="<?php bloginfo('template_url'); ?>/images/post-thumb-606x262-1.jpg">
+              <img height="114" width="195" title="" alt="" src="<?php bloginfo('template_url'); ?>/images/menu-4col-195x114.jpg" />
+            </a>
+          </p>
+          <div class="excerpt">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. </p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+        </li>
+        <li class="c-12 dashed clearfix"></li>
+        <li class="c-3 four-column clearfix">
+          <!-- <p class="meta"><a class="category" title="" href="#">Breakfast</a></p> -->
+          <!-- <h3 class="title"><a href="">Holiday Casseroles 1</a></h3> -->
+          <p class="image">                        	
+            <a class="lightbox" href="<?php bloginfo('template_url'); ?>/images/post-thumb-606x262-1.jpg">
+              <img height="114" width="195" title="" alt="" src="<?php bloginfo('template_url'); ?>/images/menu-4col-195x114.jpg" />
+            </a>
+          </p>
+          <div class="excerpt">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. </p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+        </li>
+        <li class="c-3 four-column">
+          <!-- <p class="meta"><a class="category" title="" href="#">Breakfast</a></p> -->
+          <!-- <h3 class="title"><a href="">Holiday Casseroles 1</a></h3> -->
+          <p class="image">                        	
+            <a class="lightbox" href="<?php bloginfo('template_url'); ?>/images/post-thumb-606x262-1.jpg">
+              <img height="114" width="195" title="" alt="" src="<?php bloginfo('template_url'); ?>/images/menu-4col-195x114.jpg" />
+            </a>
+          </p>
+          <div class="excerpt">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. </p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+        </li>
+        <li class="c-3 four-column">
+          <!-- <p class="meta"><a class="category" title="" href="#">Breakfast</a></p> -->
+          <!-- <h3 class="title"><a href="">Holiday Casseroles 1</a></h3> -->
+          <p class="image">                        	
+            <a class="lightbox" href="<?php bloginfo('template_url'); ?>/images/post-thumb-606x262-1.jpg">
+              <img height="114" width="195" title="" alt="" src="<?php bloginfo('template_url'); ?>/images/menu-4col-195x114.jpg" />
+            </a>
+          </p>
+          <div class="excerpt">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. </p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+        </li>
+        <li class="c-3 four-column">
+          <!-- <p class="meta"><a class="category" title="" href="#">Breakfast</a></p> -->
+          <!-- <h3 class="title"><a href="">Holiday Casseroles 1</a></h3> -->
+          <p class="image">                        	
+            <a class="lightbox" href="<?php bloginfo('template_url'); ?>/images/post-thumb-606x262-1.jpg">
+              <img height="114" width="195" title="" alt="" src="<?php bloginfo('template_url'); ?>/images/menu-4col-195x114.jpg" />
+            </a>
+          </p>
+          <div class="excerpt">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. </p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+        </li>
+        <li class="c-12 dashed clearfix"></li>
+        <li class="c-3 four-column clearfix">
+          <!-- <p class="meta"><a class="category" title="" href="#">Breakfast</a></p> -->
+          <!-- <h3 class="title"><a href="">Holiday Casseroles 1</a></h3> -->
+          <p class="image">                        	
+            <a class="lightbox" href="<?php bloginfo('template_url'); ?>/images/post-thumb-606x262-1.jpg">
+              <img height="114" width="195" title="" alt="" src="<?php bloginfo('template_url'); ?>/images/menu-4col-195x114.jpg" />
+            </a>
+          </p>
+          <div class="excerpt">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. </p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+        </li>
+        <li class="c-3 four-column">
+          <!-- <p class="meta"><a class="category" title="" href="#">Breakfast</a></p> -->
+          <!-- <h3 class="title"><a href="">Holiday Casseroles 1</a></h3> -->
+          <p class="image">                        	
+            <a class="lightbox" href="<?php bloginfo('template_url'); ?>/images/post-thumb-606x262-1.jpg">
+              <img height="114" width="195" title="" alt="" src="<?php bloginfo('template_url'); ?>/images/menu-4col-195x114.jpg" />
+            </a>
+          </p>
+          <div class="excerpt">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. </p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+        </li>
+        <li class="c-3 four-column">
+          <!-- <p class="meta"><a class="category" title="" href="#">Breakfast</a></p> -->
+          <!-- <h3 class="title"><a href="">Holiday Casseroles 1</a></h3> -->
+          <p class="image">                        	
+            <a class="lightbox" href="<?php bloginfo('template_url'); ?>/images/post-thumb-606x262-1.jpg">
+              <img height="114" width="195" title="" alt="" src="<?php bloginfo('template_url'); ?>/images/menu-4col-195x114.jpg" />
+            </a>
+          </p>
+          <div class="excerpt">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. </p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+        </li>
+        <li class="c-3 four-column">
+          <!-- <p class="meta"><a class="category" title="" href="#">Breakfast</a></p> -->
+          <!-- <h3 class="title"><a href="">Holiday Casseroles 1</a></h3> -->
+          <p class="image">                        	
+            <a class="lightbox" href="<?php bloginfo('template_url'); ?>/images/post-thumb-606x262-1.jpg">
+              <img height="114" width="195" title="" alt="" src="<?php bloginfo('template_url'); ?>/images/menu-4col-195x114.jpg" />
+            </a>
+          </p>
+          <div class="excerpt">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. </p>
+          </div>
+          <p class="actions"><a title="" href="" class="read-more-red">Read more</a></p>
+        </li>
+      </ul>
+        
+      <div class="pagination">
+        <ul>
+          <li><a class="previous" href="#">Previous</a></li>
+          <li><a class="current" href="#">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+          <li><a href="#">4</a></li>
+          <li><a href="#">5</a></li>
+          <li><a class="next" href="#">Next</a></li>
+        </ul>
+      </div><!-- end pagination -->
+    </div>    
+  </div><!-- end wrap -->
+</div><!-- end content -->
+
+<?php get_footer(); ?>
+
