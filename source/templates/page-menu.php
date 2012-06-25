1d5228ff5157e83916bc59af6ce0eae59420064d
Joshua Abbott <joshnabbott@gmail.com>
2012-06-24 20:29:13 -0700

    Adding menu page.

---
+++ source/templates/page-menu.php
@@ -0,0 +1,163 @@
+<?php get_header(); ?>
+
+<div id="intro">
+  <div class="wrap">
+    <div class="c-8">
+      <h1>Menu</h1>
+      <p class="breadcrumbs">You are here: <a href="home.html">Home</a> &raquo; <strong>Menu</strong> </p>
+    </div>
+    
+    <div class="c-4">
+      <ul class="social-bookmarking">
+        <li><a class="tw" href=""></a></li>
+        <li><a class="fb" href=""></a></li>
+        <li><a class="in" href=""></a></li>
+        <li><a class="rss" href=""></a></li>
+      </ul>
+    </div>
+  </div><!-- end wrap -->
+</div><!-- end intro -->
+
+<div id="content">
+  <div class="wrap">
+    <div class="c-6">
+      <h2 class="title">H2 It's okay to make a mess, experiments can lead to beautiful things.</h2>
+      <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem vehicula vitae. facilisis vel, accumsan a, sapien. Vivamus convallis rutrum lorem. Praesent massa lectus, vulputate sed, vestibulum eu, vestibulum eu, dolor.</p>
+      <p>Integer ante. Proin ac odio in urna cursus ultricies. Quisque tincidunt lacus in ante. Donec adipiscing. Curabitur at enim at nulla iaculis feugiat. Ut semper, nisi vel suscipit venenatis, felis diam auctor mi, quis iaculis leo nulla vel sapien. 
+Phasellus blandit nibh vel turpis. Curabitur commodo.</p>
+    </div>
+        
+    <div class="c-6">
+      <div class="menu-off-the-day">
+        <h3 class="title">Menu Of The Day</h3>
+        <ul>
+          <li><h2 class="title"><a href="">Keeping it Easy and Real</a><span>$12.3</span></h2></li>
+          <li><h2 class="title"><a href="">Creamy Chicken Mains</a><span>$12.3</span></h2></li>
+          <li><h2 class="title"><a href="">Oven Sweet Potato Fries</a><span>$12.3</span></h2></li>
+          <li><h2 class="title"><a href="">Holiday Casseroles</a><span>$12.3</span></h2></li>
+        </ul>
+      </div>
+    </div>
+        
+    <div id="price-category">
+      <div class="c-6">	
+        <h3 class="price-category-heading">Breakfast</h3>
+        <p class="actions"><a href="">view more</a></p>
+      
+        <ul class="price-items">
+          <li>
+            <div class="item-image">
+              <p class="image"><img src="assets/price-menu-82x82-1.jpg" width="82" height="82" alt="" title="" /></p>
+              <a class="specialty" href="">Specialty</a>
+            </div>
+            <div class="price-heading">
+              <h2><a href="">Holiday Casseroles</a></h2>
+              <span>$12.3</span>
+            </div>
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan a, sapien. </p>
+          </li>	
+          <li>
+            <div class="item-image">
+              <p class="image"><img src="assets/price-menu-82x82.jpg" width="82" height="82" alt="" title="" /></p>
+            </div>
+            <div class="price-heading">
+              <h2><a href="">Holiday Casseroles</a></h2>
+              <span>$12.3</span>
+            </div>
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan a, sapien. </p>
+          </li>		
+        </ul>
+      </div>   
+
+      <div class="c-6">	
+        <h3 class="price-category-heading"> Lunch</h3>
+        <p class="actions"><a href="">view more</a></p>
+        
+        <ul class="price-items">
+          <li>
+            <div class="item-image">
+              <p class="image"><img src="assets/price-menu-82x82-1.jpg" width="82" height="82" alt="" title="" /></p>
+              <a class="specialty" href="">Specialty</a>
+            </div>
+            <div class="price-heading">
+              <h2><a href="">Holiday Casseroles</a></h2>
+              <span>$12.3</span>
+            </div>
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan a, sapien. </p>
+          </li>	
+          <li>
+            <div class="item-image">
+              <p class="image"><img src="assets/price-menu-82x82.jpg" width="82" height="82" alt="" title="" /></p>
+            </div>
+            <div class="price-heading">
+              <h2><a href="">Holiday Casseroles</a></h2>
+              <span>$12.3</span>
+            </div>
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan a, sapien. </p>
+          </li>		
+        </ul>
+      </div>  
+
+      <div class="c-6 clearfix">	
+        <h3 class="price-category-heading"> Dinner</h3>
+        <p class="actions"><a href="">view more</a></p>
+        
+        <ul class="price-items">
+          <li>
+            <div class="item-image">
+              <p class="image"><img src="assets/price-menu-82x82-1.jpg" width="82" height="82" alt="" title="" /></p>
+              <a class="specialty" href="">Specialty</a>
+            </div>
+            <div class="price-heading">
+              <h2><a href="">Holiday Casseroles</a></h2>
+              <span>$12.3</span>
+            </div>
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan a, sapien. </p>
+          </li>	
+          <li>
+            <div class="item-image">
+              <p class="image"><img src="assets/price-menu-82x82.jpg" width="82" height="82" alt="" title="" /></p>
+            </div>
+            <div class="price-heading">
+              <h2><a href="">Holiday Casseroles</a></h2>
+              <span>$12.3</span>
+            </div>
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan a, sapien. </p>
+          </li>		
+        </ul>
+      </div>  
+
+      <div class="c-6">	
+        <h3 class="price-category-heading"> Dessert</h3>
+        <p class="actions"><a href="">view more</a></p>
+        
+        <ul class="price-items">
+          <li>
+            <div class="item-image">
+              <p class="image"><img src="assets/price-menu-82x82-1.jpg" width="82" height="82" alt="" title="" /></p>
+              <a class="specialty" href="">Specialty</a>
+            </div>
+            <div class="price-heading">
+              <h2><a href="">Holiday Casseroles</a></h2>
+              <span>$12.3</span>
+            </div>
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan a, sapien. </p>
+          </li>	
+          <li>
+            <div class="item-image">
+              <p class="image"><img src="assets/price-menu-82x82.jpg" width="82" height="82" alt="" title="" /></p>
+            </div>
+            <div class="price-heading">
+              <h2><a href="">Holiday Casseroles</a></h2>
+              <span>$12.3</span>
+            </div>
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan a, sapien. </p>
+          </li>		
+        </ul>
+      </div>   
+    </div><!-- end price-category -->
+  </div><!-- end wrap -->
+</div><!-- end content -->
+
+<?php get_footer(); ?>
+
